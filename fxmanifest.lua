d648130684675675b8f5e21ce2df79f745a251f5
xKurizu <139591961+xKurizuSan@users.noreply.github.com>
2024-09-08 12:38:22 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "cerulean"
+game "gta5"
+author 'TayMcKenzieNZ'
+
+file "sp_manifest.ymt"
+data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "sp_manifest.ymt"
+
+
+files {
+	'events.meta',
+	'relationships.dat'
+}
+
+data_file 'FIVEM_LOVES_YOU_4B38E96CC036038F' 'events.meta'
+
+
+server_script 'server.lua'
+client_script 'client.lua'
+
+print("Server Scenarios by TayMcKenzieNZ.")
